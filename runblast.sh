54bf272d1986aa57622873eda3465796256aedbc
Ricardo Avila <ricardo.x.avilaescobedo@gsk.com>
2020-02-08 17:13:00 -0500

    Add shell parameters

--- runblast.sh
+++ runblast.sh
@@ -1,2 +1,4 @@
-blastp -db fasta.fa -query database.fa \
-    -outfmt "6 std stitle qcovs" -num_threads 10 -out out.blast
+#!/bin/sh
+
+blastp -db $1 -query $2 -outfmt "6 std stitle qcovs" -num_threads 10 -out out.blast
+
