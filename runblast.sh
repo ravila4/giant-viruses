5ec7c44446f219d158ef9cc718c45a3a39cc2d7f
Ricardo Avila <ricardo.x.avilaescobedo@gsk.com>
2020-02-08 17:39:07 -0500

    Create blast database and other stuff

--- runblast.sh
+++ runblast.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-blastp -db $1 -query $2 -outfmt "6 std stitle qcovs" -num_threads 10 -out out.blast
+blastp -db $1 -query $2 -outfmt "6 std stitle qcovs" -num_threads 4 -out out.blast
 
